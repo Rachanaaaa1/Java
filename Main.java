a5abf458f4a597786cdebcc448742d0e78a13676
Rachanaaaa1 <117339694+Rachanaaaa1@users.noreply.github.com>
2025-07-17 13:43:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //output
+        System.out.print("Hi!! Hello \n");
+        System.out.print("Hi!! Hello");
+        System.out.println("Hi!! Hello");
+    }
+    
+}
---
+++ Star.java
@@ -0,0 +1,13 @@
+// public class Star {
+    
+//     public static void main(String args[]){
+//         System.out.print("*\n**\n***\n****");
+//     }
+// }
+
+public class Star {
+    
+    public static void main(String[] args){
+        System.out.print("***\n");
+    }
+}
