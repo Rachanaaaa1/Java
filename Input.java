b49a416caf989ebf081f09ee224eea8b8e6f5cd6
Rachanaaaa1 <117339694+Rachanaaaa1@users.noreply.github.com>
2025-07-17 14:00:23 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // next takes only one token
+        // String name = sc.next();
+        // System.out.println(name);
+
+        String mane = sc.nextLine();
+        System.out.println(mane);
+
+        //nextInt()
+        //nextfloat()
+        //nextDouble()
+
+    }
+}
